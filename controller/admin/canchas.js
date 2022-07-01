c2a10b683e2d7798913e2b76617be209c190a3fd
Lourdes90Cabrera <101159906+Lourdes90Cabrera@users.noreply.github.com>
2022-07-01 00:00:15 -0600

    agregando apartados de cancha a git

---
+++ controller/admin/canchas.js
@@ -0,0 +1,9 @@
+const API_CANCHA = SERVER + 'admin/canchas.php?action=';
+
+var modal = new bootstrap.Modal(document.getElementById("modal-canchas"), {
+    keyboard: false
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    readRows
+});
